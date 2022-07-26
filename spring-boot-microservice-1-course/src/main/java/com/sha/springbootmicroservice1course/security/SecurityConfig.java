5778e93b1d42385dc01726af5748ef5a3e372b9a
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-07-26 11:18:40 +0200

    Oppgradert etter at WebSecurityConfigurerAdapter ble deprecated

--- spring-boot-microservice-1-course/src/main/java/com/sha/springbootmicroservice1course/security/SecurityConfig.java
+++ spring-boot-microservice-1-course/src/main/java/com/sha/springbootmicroservice1course/security/SecurityConfig.java
@@ -2,15 +2,22 @@ package com.sha.springbootmicroservice1course.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
     @Value("${service.security.secure-key-username}")
     private String SECURE_KEY_USERNAME;
@@ -18,19 +25,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${service.security.secure-key-password}")
     private String SECURE_KEY_PASSWORD;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser(SECURE_KEY_USERNAME)
-                .password(passwordEncoder().encode(SECURE_KEY_PASSWORD))
-                .roles("USER");
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        String password = passwordEncoder().encode(SECURE_KEY_PASSWORD);
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(SECURE_KEY_USERNAME)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.csrf().disable();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .formLogin()
+                .and().httpBasic(withDefaults())
+//                .csrf()
+        ;
+        return http.build();
     }
 
     @Bean
